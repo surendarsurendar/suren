f27573042d76b5f58530c9723fe5907c49ed641d
surendarsurendar <35688306+surendarsurendar@users.noreply.github.com>
2018-01-31 16:33:03 +0530

    surendar2.c

---
+++ surendar2.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int a,b;
+printf("enter the number");
+scanf("%d,&a");
+b=a%2;
+if(b=1)
+printf("odd");
+else if(b=2)
+printf("even");
+getch;
+return 0;
+}
