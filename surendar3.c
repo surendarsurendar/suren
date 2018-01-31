dd6dd4c357beb17ed00eb1a3e77819097a0f7945
surendarsurendar <35688306+surendarsurendar@users.noreply.github.com>
2018-01-31 16:40:20 +0530

    surendar3.c

---
+++ surendar3.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+clrscr();
+char x;
+scanf("%c",&x);
+if(x==a,e,i,o,u)
+{
+printf("the letter is vowel");
+}
+else
+{
+printf("the letter is not  a vowel");
+}
+getch();
+}
