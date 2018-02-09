c2916144d9fab69fb5112d3425cff546c08bfe30
surendarsurendar <35688306+surendarsurendar@users.noreply.github.com>
2018-02-09 13:38:40 +0530

    Update surendar(feb 9).c

--- surendar(feb 9).c
+++ surendar(feb 9).c
@@ -4,11 +4,11 @@ int main()
 int n,i,k=0;
 scanf("%d",&n);
 for(i=2;i<n;i++)
-{
+   {
     if(n%i==0)
         ++k;
-}
-if(k==0)
+   }
+     if(k==0)
     printf("prime number");
 else
     printf("not a prime number");
