733521fb3ab226fb2a62b16254489e7661927ef2
surendarsurendar <35688306+surendarsurendar@users.noreply.github.com>
2018-02-21 16:26:49 +0530

    Create arymin(feb21).c

---
+++ arymin(feb21).c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+void main()
+{
+int a[100],i,min=0,n;
+printf("Enter the value of n");
+scanf("%d",&n);
+for(i=0;i<=n;i++)
+{
+printf("Enter the values");
+scanf("%d",&a[i]);
+}
+for(i=0;i<=n;i++)
+{
+  if(min>a[i])
+  {
+    min=a[i];
+  }
+}
+  printf("%d",max);
+}
