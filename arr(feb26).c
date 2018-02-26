0ba8773765d25a09813e732c0b9a10bf8de89e53
surendarsurendar <35688306+surendarsurendar@users.noreply.github.com>
2018-02-26 15:53:53 +0530

    Create arr(feb26).c

---
+++ arr(feb26).c
@@ -0,0 +1,28 @@
+
+#include<stdio.h>
+void main()
+{
+    int i,j,n,a[15],temp;
+    clrscr();
+    printf("Enter the array size\t");
+    scanf("%d\n",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d\t",&a[i]);
+    }
+    for(i=0;i<n-1;i++)
+    {
+        for(j=i+1;j<n;j++)
+        {
+            if(a[i]>a[j])
+            {
+                temp=a[i];
+                a[i]=a[j];
+                a[j]=temp;
+            }
+        }
+    }
+    printf("%d",a[n/2]);
+    getch();
+
+}
