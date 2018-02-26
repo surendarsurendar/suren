e013be616a451a68040e18a303a67ab6072cd10a
surendarsurendar <35688306+surendarsurendar@users.noreply.github.com>
2018-02-26 16:24:04 +0530

    Create feb(26).c

---
+++ feb(26).c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int i,n;
+    int sum=0,a[60];
+    int avg=0;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        sum=sum+a[i];
+    }
+avg=sum/n;
+printf("%d",avg);
+    
+}
